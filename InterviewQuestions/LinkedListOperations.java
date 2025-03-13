bd25cead569069f7a133e56ccc7ab7d5ac8fac20
AkshatGLA <tanishqsomani28@gmail.com>
2025-03-13 17:20:14 +0530

    Updated LinkedList and vcs.xml

--- InterviewQuestions/LinkedListOperations.java
+++ InterviewQuestions/LinkedListOperations.java
@@ -1,16 +1,38 @@
 package LinkedList.InterviewQuestions;
 
 public class LinkedListOperations {
-    public static Node dltNthLast(Node head,int n){
+    static void display(Node head){
+        Node temp=head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+    static int size(Node head){
         Node temp=head;
         int size=0;
         while(temp!=null){
             size++;
             temp=temp.next;
         }
-        for(int i=0;i<size-n;i++){
-
+        return size;
+    }
+    static void deleteNode(Node head){
+        Node temp=head;
+        while(temp.next.next!=null){
+            temp.data=temp.next.data;
+            temp=temp.next;
         }
+        temp.data=temp.next.data;
+        temp.next=null;
+    }
+    static Node dltNthLast(Node head,int n){
+        Node temp=head;
+        int size=size(head);
+        for(int i=0;i<size-n;i++){
+            temp=temp.next;
+        }
+        temp=temp.next;
         return temp;
     }
     public static class Node{
@@ -19,6 +41,36 @@ public class LinkedListOperations {
         Node(int data){
             this.data=data;
         }
+    }
+    static Node dltNth(Node head,int n){
+        Node temp=head;
+        if(n==1){
+            head=head.next;
+            return head;
+        }
+        if(temp.next==null){
+            temp=null;
+            return head;
+        }
+        for(int i=0;i<n;i++){
+            temp=temp.next;
+        }
+        temp.data=temp.next.data;
+        temp.next=temp.next.next;
+        return head;
+    }
+    static Node dltNthLastInOne(Node head,int n){
+        Node fast=head;
+        Node slow=head;
+        for(int i=0;i<n;i++){
+            fast=fast.next;
+        }
+        while(fast!=null){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        slow.next=slow.next.next;
+        return head;
     }
     public static void main(String[] args) {
         Node a=new Node(100);
@@ -32,6 +84,14 @@ public class LinkedListOperations {
         c.next=d;
         d.next=e;
         e.next=f;
-        System.out.println(e);
+    //    display(a);
+        // System.out.println(dltNthLast(a,3).data);
+        // deleteNode(b);
+        // display(a);
+        display(a);
+        // dltNth(a,2);
+        System.out.println();
+        display(dltNth(a, 4));
+        System.out.println(dltNthLast(a, 3));
     }
 }
--- LinkedListImplementation.java
+++ LinkedListImplementation.java
@@ -12,9 +12,85 @@ public class LinkedListImplementation {
         Node head=null;
         Node tail=null;
         int size=0;
-
-        void insertAtEnd(int data){
-            
+        int size(){
+            Node temp=head;
+            while(temp!=null){
+                temp=temp.next;
+                size++;
+            }
+            return size;
         }
+        void display(){
+            Node temp=head;
+            while(temp!=null){
+                System.out.print(temp.data+ " ");
+                temp=temp.next;
+            }
+            System.out.println();
+        }
+        void insertAtEnd(int val) {
+            Node temp=new Node(val);
+            if(head==null){
+                tail=temp;
+                head=temp;
+            }else{
+                tail.next=temp;
+                tail=temp;
+            }
+        }
+        void insertAtHead(int val){
+            Node temp=new Node(val);
+            if(head==null){
+                head=temp;
+                tail=temp;
+            }else{
+                temp.next=head;
+                head=temp;
+            }
+        }
+        void dltNode(int n){
+            Node temp=head;
+            if(n==size()){
+                for(int i=0;i<n-2;i++){
+                    temp=temp.next;
+                }
+                System.out.println(temp.data);
+                temp.next=null;
+                return;
+            }
+            if(n==0){
+                head=head.next;
+            }
+            for(int i=0;i<n-1;i++){
+                temp=temp.next;
+            }
+            temp.next=temp.next.next;
+        }
+        void getAt(int idx){
+            Node temp=head;
+            if(idx>=size()){
+                System.out.println("Index out of bound");
+                return;
+            }
+            if(idx<0){
+                System.out.println("Invalid Index");    
+                return;
+            }
+            for(int i=0;i<idx;i++){
+                temp=temp.next;
+            }
+            System.out.println(temp.data);
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll=new LinkedList();
+        ll.insertAtEnd(2);
+        ll.insertAtEnd(3);
+        ll.insertAtEnd(4);
+        ll.insertAtHead(1);
+        ll.insertAtHead(0);
+        ll.display();
+        ll.getAt(5);
     }
 }
