9ede352e78b2a8ca71257c9485e08147aae6f585
AkshatGLA <tanishqsomani28@gmail.com>
2025-03-25 14:36:04 +0530

    initial commit

---
+++ Test.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        String str1 = "ABCABC", str2 = "ABCd";
+//        System.out.println(str1.contains(str2));
+        ArrayList<Integer> candy=new ArrayList<>();
+        candy.add(1);
+        candy.add(2);
+        System.out.println(candy.get(0));
+
+        System.out.println(str1.substring(0,1));
+        System.out.println(1%7);
+        System.out.println(2%7);
+        System.out.println(3%7);
+        System.out.println(4%7);
+    }
+}
